18e79bdb2ba4577cb827eea34073301edc62c22a
hailey-tam-ics4u1-03-25 <hailey.tam@student.tdsb.on.ca>
2025-05-14 18:41:19 +0000

    Completed Code

---
+++ src/main/java/Animal.java
@@ -0,0 +1,37 @@
+ abstract class Animal{
+    protected String name;
+    protected int age;
+    protected String type;
+    protected int position;
+
+    public Animal(String name, int age, String type, int position){
+        this.name = name;
+        this.age = age;
+        this.type = type;
+        this.position = position;
+    }
+    
+    public String getName(){
+        return this.name;
+    }
+
+    public int getAge(){
+        return this.age;
+    }
+
+    public String getType(){
+        return this.type;
+    }
+
+    public int getPosition(){
+        return this.position;
+    }
+    
+    public String sleep(){
+        return "Zzz";
+    }
+
+    abstract String animalSound();
+
+    abstract void move();
+}
